579ce2404cbeea77511d5b130dd966bfcedc6c8a
Oussama Errahili <oussamaerrahili124@gmail.com>
2025-02-14 15:49:42 +0100

    mvc

--- login.php
+++ login.php
@@ -5,21 +5,6 @@ if(isset($_POST['submit'])){
     $check = new User($_POST['email'], $_POST['password']);
     $check->login();
 }
+require 'login-view.php';
 ?>
 
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form action="" method="post">
-        <input type="email" name="email" placeholder="email" id="">
-        <input type="password" name="password" placeholder="password" id="">
-        <button type="submit" name="submit">login</button>
-    </form>
-</body>
-</html>
