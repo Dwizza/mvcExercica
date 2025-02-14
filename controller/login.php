8418ba5017577c6b8df2e1e4bff71375c855f368
Oussama Errahili <oussamaerrahili124@gmail.com>
2025-02-14 16:03:43 +0100

    structure mvc

--- controller/login.php
+++ controller/login.php
@@ -1,10 +1,10 @@
 <?php 
-require_once 'db.php';
-require_once 'user.php';
+require_once '../db.php';
+require_once '../models/user.php';
 if(isset($_POST['submit'])){
     $check = new User($_POST['email'], $_POST['password']);
     $check->login();
 }
-require 'login-view.php';
+require '../views/login-view.php';
 ?>
 
--- db.php
+++ db.php
@@ -15,6 +15,4 @@ public function getConnection(){
         return $conn;
     }
 }
-
-
 }
